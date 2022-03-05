b019ca1515f26dbe92bafe8fa169d2dd10b88773
neksioo <100017725+neksioo@users.noreply.github.com>
2022-03-05 14:46:47 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.4;
+  audio.play()
+}
+
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.play()
+}

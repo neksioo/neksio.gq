f2adb99217779a137c454842d52121a7679cd0bb
neksioo <100017725+neksioo@users.noreply.github.com>
2022-03-06 10:44:33 +0100

    Update typed.js

--- typed.js
+++ typed.js
@@ -8,7 +8,7 @@
 */
 
 new Typed('#typed',{
-    strings : ['neksio','developer','vfx editor','gfx designer','ui designer','idiot','skid','big namer','pr check'],
+    strings : ['neksio','developer','vfx editor','gfx designer','ui designer','idiot','skid','big namer','pr check','faggot'],
     typeSpeed : 40,
     delaySpeed : 90,
     loop : true

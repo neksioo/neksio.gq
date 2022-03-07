f42bc4702ce796e1914ae22cb7075bf24e910c01
neksioo <100017725+neksioo@users.noreply.github.com>
2022-03-07 20:19:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝ ╚═════╝╚═╝ 
 */
 
+// !!!!!!!!!!!!! THIS WEBSITE IS FREE TO USE !!!!!!!!!!!!! 
+
 function audioPlay() {
   var audio = document.getElementById("audio");
   audio.volume = 0.4;

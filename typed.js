8cf4baaabcc3948ed4d146f5c724868de09b1152
neksioo <100017725+neksioo@users.noreply.github.com>
2022-03-07 20:19:33 +0100

    Update typed.js

--- typed.js
+++ typed.js
@@ -7,7 +7,7 @@
 ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝ ╚═════╝╚═╝ 
 */
 
-<!-- !!!!!!!!!!!!! THIS WEBSITE IS FREE TO USE !!!!!!!!!!!!! -->
+// !!!!!!!!!!!!! THIS WEBSITE IS FREE TO USE !!!!!!!!!!!!! 
 
 new Typed('#typed',{
     strings : ['neksio','developer','c#','py','html','js','css','gfx designer','vfx editor','ui/ux designer','big namer','idiot','skid/paster'],

